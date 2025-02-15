67dcd2a6b61a928fe900f450f0065f0555be393b
AndrewNella <50403759+AndrewNella@users.noreply.github.com>
2025-02-15 15:56:07 +0200

    Movement First iteration

--- Assets/_PROJECT/Scripts/PlayerController.cs
+++ Assets/_PROJECT/Scripts/PlayerController.cs
@@ -9,45 +9,55 @@ public class PlayerController : MonoBehaviour
 
     public bool isMoving;
 
-    Vector2 movementDirection;
+    Vector2 inputVector;
 
     private void Awake()
     {
-        movementDirection = Vector2.zero;
+        inputVector = Vector2.zero;
         actionMap = new PlayerActionMap();
         actionMap.Enable();
 
         actionMap.PlayerControllerMap.Movement.performed += x => OnPlayerMoveInput(x.ReadValue<Vector2>());
-        actionMap.PlayerControllerMap.Movement.canceled -= x => OnPlayerMoveInput(x.ReadValue<Vector2>());
+        actionMap.PlayerControllerMap.Movement.canceled += x => OnPlayerMoveInput(x.ReadValue<Vector2>());
 
     }
 
     private void OnPlayerMoveInput(Vector2 _incomingVector2)
     {
-        movementDirection = _incomingVector2;
+        Debug.Log("Is Moving");
+        inputVector = _incomingVector2;
     }
 
     void Update()
     {
         if (!isMoving)
         {
-            if (movementDirection != Vector2.zero)
+            //Removes Diagonal Movement
+            if (inputVector.x != 0) inputVector.y = 0;
+
+
+            if (inputVector != Vector2.zero)
             {
                 Vector3 _targetPos = transform.position;
-                _targetPos.x += _targetPos.x;
-                _targetPos.y += _targetPos.y;
+                _targetPos.x += inputVector.x;
+                _targetPos.y += inputVector.y;
+
+                StartCoroutine(Move(_targetPos));
             }
         }
     }
 
     IEnumerator Move(Vector3 _targetPosition)
     {
-        while ((_targetPosition - transform.position).sqrMagnitude > Mathf.Epsilon)
+        isMoving = true;
+        while ((_targetPosition - transform.position).sqrMagnitude > 0)
         {
             transform.position = Vector3.MoveTowards(transform.position, _targetPosition, moveSpeed * Time.deltaTime);
             yield return null;
         }
         transform.position = _targetPosition;
+        Debug.Log(isMoving);
+        isMoving = false;
     }
 
 }
